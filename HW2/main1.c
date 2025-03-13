001a3f5a9dc62e1f2efe5c0dd2fcc9ffc310c541
xuan1006 <111722123+xuan1006@users.noreply.github.com>
2025-03-13 14:25:47 +0800

    Update main1.c

--- HW2/main1.c
+++ HW2/main1.c
@@ -1,1 +1,44 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+void num(int l){
+    int n =7;
+    int cout[n]; 
+    FILE* fp;
+    fp = fopen("lotto.txt", "w");
+    time_t curtime;
+    time(&curtime);
+    srand(2025);
+    fprintf(fp, "%s", ctime(&curtime));
+    for (int i =1; i <=l; i++){
+        for ( int k =0;k <n; k++){
+            
+            cout[k] = rand() %69 + 1;
+                for ( int j =0;j <k; j++){
+                    if (cout[k] == cout[j]){
+                        k--;
+                        break;
+                    }
+                }
+                
+        }
+        fprintf(fp, "[%d]: ", i);
+        for(int a =0; a <n; a++){
+            fprintf(fp, "%d ", cout[a]);
+        }
+        fprintf(fp, "\n");
+    }
+
+    fclose (fp);
+    return;
+}
+int main()
+{
+    
+    int l;
+    printf("請輸入要的樂透份數：");
+    scanf("%d", &l);
+    num(l);
+    return 0;
+}
