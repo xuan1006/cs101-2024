efbbdec388e410f1fa3f7258f894ea9b1b4ef888
xuan1006 <bob921006@gmail.com>
2025-03-20 15:27:40 +0800

    ex

---
+++ ex3/pg52.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int func(int* i,int* j){
+	printf("in function\t,i = %d,j = %d\n", *i, *j);
+	int t;
+	t = *j;
+	*j = *i;
+	*i = t;
+	printf("in function\t,i = %d,j = %d\n", *i, *j);
+}
+
+int main(){
+	int n=1,m=2;
+	func(&n,&m);
+	printf("in main \t,n = %d,m = %d\n",n,m);
+		
+	return 0;
+}
